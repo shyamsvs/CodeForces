896ad36d1bb01a6d6fba0f6697d2b356bbd2465b
Shyam Kumar  S V <34570424+shyamsvs@users.noreply.github.com>
2020-07-08 07:22:15 +0530

    Create EhAb AnD gCd.cpp

---
+++ EhAb AnD gCd.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+void solve(){
+	ll x;
+	cin>>x;
+	cout<<"1 "<<x-1<<endl;
+	return;
+}
+
+
+int main(){
+	int test;
+	cin>>test;
+	while(test--)
+	solve();
+	return 0;
+}
