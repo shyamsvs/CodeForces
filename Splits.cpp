5c72def55ba1872b6707be457f3f677901585772
Shyam Kumar  S V <34570424+shyamsvs@users.noreply.github.com>
2020-07-22 07:39:25 +0530

    Create Splits.cpp

---
+++ Splits.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+	ll n;
+	cin>>n;
+	cout<<n/2 + 1 <<endl;
+	return;
+}
+
+
+int main(){
+	// int test;
+	// cin>>test;
+	// while(test--)
+	solve();
+	return 0;
+}
