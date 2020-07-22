060f86d2414748bcf9cbcb16c41edd8728caa1d5
Shyam Kumar  S V <34570424+shyamsvs@users.noreply.github.com>
2020-07-22 07:34:15 +0530

    Create Omkar and competation.cpp

---
+++ Omkar and competation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+string a,b,c;
+string answer;
+
+void solve(){
+    ll a;
+    cin>>a;
+    for(int i=0;i<a;i++){
+        cout<<"1 ";
+    }
+    cout<<endl;
+    return; 
+}
+
+
+int main(){
+	int test;
+	cin>>test;
+	while(test--)
+	solve();
+	return 0;
+}
