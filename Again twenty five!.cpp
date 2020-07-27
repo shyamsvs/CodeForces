e3dfdc7e1a12b7efae4ca9a79ada5b49135b63a0
Shyam Kumar  S V <34570424+shyamsvs@users.noreply.github.com>
2020-07-27 18:11:44 +0530

    Create Again twenty five!.cpp

---
+++ Again twenty five!.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve(){
+    ll a;
+	cin>>a;
+	cout<<"25"<<endl;
+    return;
+}
+
+int main(){
+	// int test;
+	// cin>>test;
+	// while(test--)
+	solve();
+	return 0;
+}
